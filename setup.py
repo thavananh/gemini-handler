d73aa32aa4baafe0eedeb87f2b0f5a9b1e01c899
Nguyễn Võ Minh Duy <46448653+thavananh@users.noreply.github.com>
2025-05-02 17:19:38 +0700

    Update setup.py To Install on Py311
    This mean this package can run on Kaggle

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.12",
+    python_requires=">=3.11",
     entry_points={
         'console_scripts': [
             'gemini-server=gemini_handler.cli:main',
